cb72446992db5b4d7a9a092f0df346cac35c6897
gubanov <arteumg@mail.ru>
2019-02-08 09:56:12 +0300

    Удаление java-файла

--- srs/ru/reksoft/gubanov/gittest/HelloWorld1.java
+++ srs/ru/reksoft/gubanov/gittest/HelloWorld1.java
@@ -5,6 +5,6 @@ package ru.reksoft.gubanov.gittest;
  */
 public class HelloWorld1 {
     public static void main(String[] args) {
-        System.out.println("Hello, World 1");
+        System.out.println("Hello, World 2");
     }
 }
